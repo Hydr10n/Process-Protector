a2ed48b14e6561bcaac5211a306012b14f147f04
Xun Yang <Programmer-Yang_Xun@outlook.com>
2020-11-01 10:28:37 +0800

    Fix a bug.

--- HookLib/dllmain.cpp
+++ HookLib/dllmain.cpp
@@ -2,22 +2,28 @@
 #include "ProcessProtector.h"
 
 #pragma data_seg("SharedData")
-HHOOK hHook{};
-struct { DWORD dwHookCallerProcessId; } sharedData{};
+struct {
+	HHOOK hHook;
+	DWORD dwHookCallerProcessId;
+} sharedData{};
 #pragma data_seg()
 #pragma comment(linker, "/SECTION:SharedData,RWS")
 
 HINSTANCE hInstance;
 
 BOOL WINAPI SetGlobalWindowsHook() {
-	sharedData.dwHookCallerProcessId = GetCurrentProcessId();
-	return hHook == NULL ? (hHook = SetWindowsHookExW(WH_GETMESSAGE, [](int nCode, WPARAM wParam, LPARAM lParam) { return CallNextHookEx(NULL, nCode, wParam, lParam); }, hInstance, 0)) != NULL : FALSE;
+	BOOL ret = sharedData.hHook == NULL;
+	if (ret) {
+		sharedData.dwHookCallerProcessId = GetCurrentProcessId();
+		ret = (sharedData.hHook = SetWindowsHookExW(WH_GETMESSAGE, [](int nCode, WPARAM wParam, LPARAM lParam) { return CallNextHookEx(NULL, nCode, wParam, lParam); }, hInstance, 0)) != NULL;
+	}
+	return ret;
 }
 
 BOOL WINAPI UnhookGlobalWindowsHook() {
-	const BOOL ret = UnhookWindowsHookEx(hHook);
+	const BOOL ret = UnhookWindowsHookEx(sharedData.hHook);
 	if (ret)
-		hHook = NULL;
+		sharedData.hHook = NULL;
 	return ret;
 }
 
@@ -25,8 +31,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpvReserved) {
 	using Hydr10n::ProcessUtils::ProcessProtector;
 	switch (dwReason) {
 	case DLL_PROCESS_ATTACH: {
-		ProcessProtector::Hide(sharedData.dwHookCallerProcessId);
-		ProcessProtector::Protect(sharedData.dwHookCallerProcessId);
+		if (sharedData.hHook != NULL) {
+			ProcessProtector::Hide(sharedData.dwHookCallerProcessId);
+			ProcessProtector::Protect(sharedData.dwHookCallerProcessId);
+		}
 		hInstance = hModule;
 	}	break;
 	}
